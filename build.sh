dfcf4d91c6265a0b2daa331362c0885963043ae9
Lukasz Idkowiak <lukasz.idkowiak@guestline.com>
2021-08-05 16:31:03 +0200

    scripts cleanup

---
+++ build.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+npm install && \
+bower install && \
+grunt clean && \
+grunt --force
--- buildAndPush.bat
+++
@@ -1,1 +0,0 @@
-grunt clean && grunt --force && git add . && git commit -a -m "Build for deploy on `date`" && git push
