84629ea869e2350ac5352e382b3884a438f7890b
Lukasz Idkowiak <lukasz.idkowiak@guestline.com>
2021-08-05 16:52:12 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
 
-grunt clean && \
-grunt && \
-git add . && \
-git commit -a -m "Build for deploy on `date`" && \
-git push && \
-ssh -p '59184' 'lukaszi@cyber-folks.pl' 'cd ~/repo/terapeuta && git pull'
+git subtree push --prefix dist origin dist
+
